e67fcde40df907e77282547a1a4660f9872f94ea
kathlyn <kathlyn@okngroup.com.br>
2025-07-07 17:54:42 -0300

    adicionando projetos, menu mobile e icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
   const webBtn = document.getElementById("web-btn");
   const uiUxBtn = document.getElementById("uiux-btn");
-  const webCourses = document.getElementById("web-courses");
-  const uiUxCourses = document.getElementById("ui-ux-courses");
+  const webProjects = document.getElementById("web-projects");
+  const uiUxProjects = document.getElementById("ui-ux-projects");
+  const menuToggle = document.getElementById("menu-toggle");
+  const headerMenu = document.getElementById("header-menu");
 
-  console.log(webBtn);
-  console.log(uiUxBtn);
-  console.log(webCourses);
-  console.log(uiUxCourses);
-  
 
-  if (webBtn && uiUxBtn && webCourses && uiUxCourses) {
+  if (webBtn && uiUxBtn && webProjects && uiUxProjects) {
     webBtn.addEventListener("click", function () {
       webBtn.classList.add("section__button--active");
       uiUxBtn.classList.remove("section__button--active");
-      webCourses.style.display = "block";
-      uiUxCourses.style.display = "none";
+      webProjects.style.display = "block";
+      uiUxProjects.style.display = "none";
     });
 
     uiUxBtn.addEventListener("click", function () {
       uiUxBtn.classList.add("section__button--active");
       webBtn.classList.remove("section__button--active");
-      uiUxCourses.style.display = "block";
-      webCourses.style.display = "none";
+      uiUxProjects.style.display = "block";
+      webProjects.style.display = "none";
+    });
+  }
+
+  if (menuToggle && headerMenu) {
+    menuToggle.addEventListener("click", function () {
+      headerMenu.classList.toggle("show");
     });
   }
 });
